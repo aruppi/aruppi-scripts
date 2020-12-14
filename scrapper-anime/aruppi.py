4bb46538bf6bd92cae63c85623f4ae8c31ee0b70
Guillermo Campanudo <guillermo.campanudo@hotmail.com>
2020-12-14 17:07:50 +0000

    Deleting no used imports

--- scrapper-anime/aruppi.py
+++ scrapper-anime/aruppi.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from urllib.parse import urlparse, unquote
 from os import path
 import os
